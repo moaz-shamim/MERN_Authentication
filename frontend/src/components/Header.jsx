d29b829d4cdb0c66d49d746b8d24248e25717f58
moaz-shamim <moaz.shamim1575@gmail.com>
2024-03-01 20:00:41 +0530

    add little css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,23 +6,31 @@ const Header = () => {
   return (
     <div className="bg-slate-200 ">
       <div className="flex justify-between items-center max-w-6xl  mx-auto p-3">
-        <h1 className="font-bold">Auth App</h1>
-        <ul className="flex gap-4">
+        <h1 className="text-2xl font-bold text-gray-800 md:text-3xl">
+          Auth App
+        </h1>
+        <ul className="flex gap-4 ">
           <Link to="/">
-            <li>Home</li>
+            <li className=" hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
+              Home
+            </li>
           </Link>
           <Link to="/about">
-            <li>About</li>
+            <li className=" hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
+              About
+            </li>
           </Link>
           <Link to="/profile">
             {currentUser ? (
               <img
                 src={currentUser.profilePicture}
                 alt="profile"
-                className="h-7 w-7 rounded-full object-cover"
+                className="h-9 w-9 rounded-full object-cover"
               />
             ) : (
-              <li>Sign In</li>
+              <li className=" hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
+                Sign In
+              </li>
             )}
           </Link>
         </ul>
